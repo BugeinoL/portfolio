4adb9815c4fdde7e128bb5c8813caca496a052f5
Arecinol <betrancourt.lonicera@gmail.com>
2022-03-27 21:18:56 +0200

    Création de dossiers

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Dancing+Script&family=Italianno&family=Stick+No+Bills:wght@200&family=Yanone+Kaffeesatz:wght@300&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="./img/logo_small_icon_only_inverted.png" type="image">
+    <link rel="stylesheet" href="./sass/index.css">
+    <title>Lonicera</title>
+</head>
+<body>
+
+
