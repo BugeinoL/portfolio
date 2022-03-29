135f4cdc2655c6fe43943dc3e1875894dca67f5e
Arecin0L <betrancourt.lonicera@gmail.com>
2022-03-29 19:27:06 +0200

    accueil + menu

--- index.php
+++ index.php
@@ -2,33 +2,30 @@
 require './include/header.php';
 ?>
 
-    <header>
-        <nav>
-            <div class="allnav">
-                <div class="fnav">
-                    <a class="home" href="Home">Home</a>
-                    <a class="idk" href="idk">IDK</a>
-                    <a class="search" href="CV">CV</a>
-                </div>
-                <img class="logo" src="./img/logo_white_large.png" alt="Logo">
-                <div class="snav">
-                    <a class="faq" href="FAQ">FAQ</a>
-                    <a class="am" href="Sing in">Sing in</a>
-                    <a class="contact" href="Contact">Contact</a>
-                </div>
-            </div>
-        </nav>
-    </header>
+<main>
+    <img class="logo" src="./img/wolf.png" alt="">
+    <div class="menu">
+        <a class="home all" href="#">Home</a>
+        <a class="cv all" href="./pages/cv.php">CV</a>
+        <a class="about all" href="#">A propos</a>
+    </div>
+    <div class="projets">
+        <a class="opal all" href="#op">Opal'Info</a>
+        <a class="truc all" href="#tr">truC</a>
+        <a class="nain all" href="#na">Nainstitution</a>
+    </div>
+</main>
 
-
-
-<!-- <img class="forest" src="./img/forest_bg.jpg" alt=""> -->
-<section>
-    
-    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur provident dolore, sed mollitia amet et laborum vel numquam placeat voluptas porro quae tempore aut officia eaque eos. Voluptates officia dignissimos cum maiores quam totam, quisquam consectetur excepturi rerum sequi officiis reiciendis, aliquid, impedit libero dolor! Unde fugit iure dicta distinctio nulla. A similique possimus eligendi cumque perferendis sit ipsa consequatur temporibus delectus omnis obcaecati, deleniti ex quod. Vel, dolor? Vero dicta facere sed, harum magnam quod laudantium ducimus provident in officiis non cupiditate minus porro consectetur quis, debitis officia quae tenetur reiciendis architecto deleniti sint ab nihil. Quibusdam, at inventore. Quidem quod nisi excepturi ducimus dolore, quam minima aliquam vitae ipsum repudiandae. Ipsum neque, illo cupiditate amet molestiae aspernatur reiciendis qui expedita incidunt consequatur ratione quam ut distinctio eligendi excepturi? Impedit adipisci placeat amet dolorum, rerum doloribus nam obcaecati libero consequuntur quis voluptatem, commodi veniam cumque eum tempore labore. Necessitatibus obcaecati natus recusandae inventore, laboriosam laborum, dolorem at eaque omnis nam facilis, iure possimus nihil tempore debitis hic amet eos sunt nulla. Reprehenderit maxime voluptates minus officiis? Accusantium officia, est libero recusandae natus nisi magnam itaque delectus commodi consequuntur dolor odio magni architecto dicta enim consectetur, sapiente possimus reiciendis sit harum vero nihil, quidem perspiciatis repellendus! Provident sit, id numquam, consectetur omnis accusamus ab quas labore saepe nulla dolores dolor nobis magnam tenetur dicta voluptatum at. Sit ullam ea deleniti! Cum obcaecati nostrum rem animi minima consequuntur explicabo magnam praesentium, voluptatibus laborum voluptates quae voluptas veniam consectetur ab reprehenderit dolorum soluta incidunt tempore aperiam. Molestiae minima sit quam. Veritatis veniam amet sed earum odit asperiores culpa ex eius explicabo pariatur totam et sint saepe, similique odio! Itaque et numquam dignissimos voluptatibus! Excepturi magni odit, porro non laboriosam repellendus fugiat distinctio, quae ducimus pariatur obcaecati similique accusantium harum quo saepe culpa provident sint omnis itaque ullam error? Beatae fuga quia expedita animi? Pariatur dolorum ab non quisquam nesciunt veniam. Voluptates consequatur illo deleniti molestias quae magnam, quo corporis omnis molestiae necessitatibus officiis amet sed maiores atque esse assumenda ipsam alias? Doloribus magni veniam id nobis pariatur autem, nihil praesentium placeat excepturi earum ea reiciendis asperiores tempora itaque perspiciatis numquam tempore aliquam ipsum! Soluta provident nihil hic sapiente dicta ratione, nisi ipsam maiores eveniet quod, vitae sit, quibusdam sint qui impedit! Debitis fugiat adipisci praesentium tenetur nesciunt aliquid in est, deserunt quis temporibus atque non obcaecati molestiae sed ducimus officiis odit! Dignissimos et est voluptatibus totam modi magnam, expedita, tenetur consequuntur cupiditate, nostrum quidem natus nesciunt minus. Delectus nobis molestiae laboriosam eaque error esse maiores repellendus architecto alias ipsum perspiciatis veniam non adipisci, a odio, itaque provident consequatur. Deleniti cumque in dolorem sequi incidunt iste eum fugiat, dolore fugit natus doloremque at, a deserunt perspiciatis hic quo quidem et unde! Accusantium iure culpa quod velit ex asperiores, in pariatur modi dolores impedit libero a animi consequuntur corporis aut quisquam provident et tempore dolore repudiandae adipisci explicabo? Maxime, numquam eveniet. Veniam, distinctio voluptatibus! Rerum quisquam atque quidem neque ea, quis voluptatum iure optio!</p>
-    
-    
+<section id="op" class="pfolio">
+    <h1>Opal'Info</h1>
 </section>
+<section id="tr" class="pfolio">
+    <h1>truC</h1>
+</section>
+<section id="na" class="pfolio">
+    <h1>Nainsitution</h1>
+</section>
+
 
 
 <?php
---
+++ pages/cv.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../sass/cv.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@100;400&family=Oswald:wght@200&display=swap" rel="stylesheet">
+    <meta name="description" content="Bonjour, je m'appelle Lonicera et je viens tout juste de terminer ma formation de développeur web.">
+    <meta name="author" content="Bétrancourt Lonicera">
+    <title>Bétrancourt Lonicera</title>
+</head>
+<body>
+    <section id="left">
+        <pre class="code-block">
+            <code class="code">
+                <span class="code-line acc"> {</span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">nom</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <span class="code-desc">Bétrancourt Lonicera</span>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">metier</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <span class="code-desc">Développeur Web</span>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">adresse</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <span class="code-desc">22 route de longvilliers 62630 Frencq</span>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">telephone</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <span class="code-desc">06.28.37.88.75</span>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">e-mail</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <a class="code-desc-lien" href="mailto:betrancourt.lonicera@gmail.com"><span class="code-desc-lien">betrancourt.lonicera@gmail.com</span></a>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> -</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">formations</span>
+                    <span class="code-ap"> {</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .... </span>
+                    <span class="code-date">2021-2022</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <span class="code-desc">Formation Développeur Web et Web Mobile</span>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .... </span>
+                    <span class="code-date">2019-2020</span>
+                    <span class="code-ap">:&nbsp"</span>
+                    <span class="code-desc">Baccalauréat STMG spécialité Gestion Finance</span>
+                    <span class="code-ap">",</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-ap">}</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> -</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-title">experiences</span>
+                    <span class="code-ap"> {</span>
+                </span>
+                <span class="code-line">
+                    <span class="code-point"> .. </span>
+                    <span class="code-ap">}</span>
+                </span>
+                
+                <span class="code-line acc"> }</span>
+            </code>
+        </pre>
+
+    </section>
+
+    <section id="right">
+
+    </section>
+</body>
+</html>
